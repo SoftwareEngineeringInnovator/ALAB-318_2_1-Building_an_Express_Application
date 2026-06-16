f97a545085deafccbd9f95173787f0f1d43085b0
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-06-16 05:32:37 -0400

    add EJS view and test in the browser

--- index.js
+++ index.js
@@ -9,12 +9,39 @@ const app = express();
 // Set port number
 const port = 3000;
 
-// Create route to test that the server works
+// Files from the public folder: CSS
+app.use(express.static("public"));
+
+// Set EJS
+app.set("view engine", "ejs");
+
+// Cybersecurity tips data that will be sent to the EJS view
+let cyberTips = [
+    "Use strong passwords.",
+    "Turn on multi-factor authentication.",
+    "Do not click suspicious links.",
+    "Keep your software updated."
+];
+
+// Home route that renders the index.ejs file
 app.get("/", (req, res) => {
-    res.send("CyberSafe Express App is running.");
+    res.render("index", {
+        title: "CyberSafe Express App",
+        message: "Welcome to CyberSafe Express App",
+        cyberTips: cyberTips
+    });
 });
 
 // Start the server
 app.listen(port, () => {
     console.log("Listening on port: " + port);
 });
+
+
+// Test Connectivity Code
+
+// * Create route to test that the server works
+// app.get("/", (req, res) => {
+//     res.send("CyberSafe Express App is running.");
+// });
+
